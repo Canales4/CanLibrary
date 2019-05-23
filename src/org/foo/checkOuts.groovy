eef7a52adb495706247c4ac361011d3d6515fca4
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-23 11:43:58 +0200

    modificando clase CheckOuts

--- src/org/foo/checkOuts.groovy
+++ src/org/foo/checkOuts.groovy
@@ -1,14 +1,14 @@
 package org.foo
-
-
-class checkOuts {
+class CheckOuts implement Serializable{
+  def steps
+  CheckOuts(steps) {this.steps = steps}
 
   // But first that use this method you should make credentialsId
   def checkOutGithubSSH(user, repo, nameCred, branch) {
     if(branch != null){
-      git branch: "${branch}", credentialsId: "${nameCred}", url: "git@github.com:${user}/${repo}.git"
+      steps.git "branch: ${branch}, credentialsId: ${nameCred}, url: git@github.com:${user}/${repo}.git"
     }else{
-      git credentialsId: "${nameCred}", url: "git@github.com:${user}/${repo}.git"
+      steps.git "credentialsId: ${nameCred}, url: git@github.com:${user}/${repo}.git"
     }
   }
 }
