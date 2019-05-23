09a84b260aa901a1a63b61efccf7d4ae80c4d055
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-23 12:53:35 +0200

    añadiendo y modificando vars

---
+++ vars/checkOutGitHTTP.groovy
@@ -0,0 +1,9 @@
+#!C:\devops\Groovy-3.0.0\bin groovy
+// /vars/checkOutGitHTTP
+def call(String branch, String cred, String name, String repo){
+  if(branch != null){
+    echo "git branch: ${branch}, credentialsId: ${cred}, url: https://github.com/${name}/${repo}.git"
+  } else {
+    echo "git credentialsId: ${cred}, url: https://github.com/${name}/${repo}.git"
+  }
+}
--- vars/checkOutGitSSH.groovy
+++ vars/checkOutGitSSH.groovy
@@ -1,7 +1,9 @@
 #!C:\devops\Groovy-3.0.0\bin groovy
-
 // vars/checkOutGitSSH
-
 def call(String branch, String cred, String name, String repo){
-  echo "git branch: ${branch}, credentialsId: ${cred}, url: git@github.com:${name}/${repo}.git"
+  if(branch != null){
+    echo "git branch: ${branch}, credentialsId: ${cred}, url: git@github.com:${name}/${repo}.git"
+  } else {
+    echo "git credentialsId: ${cred}, url: git@github.com:${name}/${repo}.git"
+  }
 }
