44b04f5e373fa171d56dbc53f575f72b4a09f245
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-23 12:07:27 +0200

    modificando el pipeline

--- vars/CheckOuts.groovy
+++ vars/CheckOuts.groovy
@@ -1,3 +1,4 @@
+import java.lang.*
 def checkOutSSHGit(String branch, String credId,String name, String repo){
   echo "git branch: ${branch}, credentialsId: ${credId}, url: git@github.com:${name}/${repo}.git"
 }
