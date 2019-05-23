6610a3aef028c8285624838e8fc8d04937ecb4c5
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-23 11:26:39 +0200

    añadiendo estructura de la libreria y una clase para SCM

--- src/org/foo/checkOuts.groovy
+++ src/org/foo/checkOuts.groovy
@@ -6,7 +6,7 @@ class checkOuts {
   // But first that use this method you should make credentialsId
   def checkOutGithubSSH(user, repo, nameCred, branch) {
     if(branch != null){
-      git branch: "${branch}", credentialsId: "${nameCred}", url: "git@github.com:${user}/${repo}.git"
+      echo "git branch: "${branch}", credentialsId: "${nameCred}", url: git@github.com:"${user}"/"${repo}".git"
     }else{
       git credentialsId: "${nameCred}", url: "git@github.com:${user}/${repo}.git"
     }
