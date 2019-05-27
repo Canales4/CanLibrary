7dcf8bc1e3be0fad6f8125e781f7d098d3f0b3d1
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-27 09:51:59 +0200

    modificando las vars

--- vars/checkOutGitSSH.groovy
+++ vars/checkOutGitSSH.groovy
@@ -1,6 +1,6 @@
 #!C:\devops\Groovy-3.0.0\bin groovy
 // vars/checkOutGitSSH
-def call(String branch, String cred, String name, String repo){
+def call(String branch = null, String cred, String name, String repo){
   if(branch != null){
     echo "git branch: ${branch}, credentialsId: ${cred}, url: git@github.com:${name}/${repo}.git"
   } else {
