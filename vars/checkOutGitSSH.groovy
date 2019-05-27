48f9709930676c0f4ac0248dd2d73b590b29f14a
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-27 11:39:44 +0200

    modi vars

--- vars/checkOutGitSSH.groovy
+++ vars/checkOutGitSSH.groovy
@@ -2,7 +2,7 @@
 // vars/checkOutGitSSH
 def call(String branch = null, String cred, String name, String repo){
   if(branch != null){
-    git branch: '${branch}', credentialsId: '${cred}', url: git@github.com:${name}/${repo}.git
+    git branch: ${branch}, credentialsId: ${cred}, url: git@github.com:${name}/${repo}.git
   } else {
     git credentialsId: '${cred}', url: "git@github.com:'${name}'/'${repo}'.git"
   }
