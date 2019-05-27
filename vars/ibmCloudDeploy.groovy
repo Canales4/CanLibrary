e6f1ed9edb30fe5204ea3dc67f10984529212f70
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-27 13:01:03 +0200

    modificando un $

--- vars/ibmCloudDeploy.groovy
+++ vars/ibmCloudDeploy.groovy
@@ -3,5 +3,5 @@
 
 //this var will only work if the client and the server go separator in the same directory
 def call(String app, String apikey) {
-  sh "ibmcloud login --apikey $${apikey} && ibmcloud target --cf && cd client && ibmcloud cf push ${app}"
+  sh "ibmcloud login --apikey ${apikey} && ibmcloud target --cf && cd client && ibmcloud cf push ${app}"
 }
