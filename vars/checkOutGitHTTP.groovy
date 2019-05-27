50b9f4dd684c9741bfd90fd163f0663d51aa1ca3
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-27 11:49:14 +0200

    modi vars

--- vars/checkOutGitHTTP.groovy
+++ vars/checkOutGitHTTP.groovy
@@ -2,8 +2,8 @@
 // /vars/checkOutGitHTTP
 def call(String branch = null, String cred, String name, String repo){
   if(branch != null){
-     git branch: '${branch}', credentialsId: '${cred}', url: 'https://github.com/${name}/${repo}.git'
+     git branch: "${branch}", credentialsId: "${cred}", url: "https://github.com/${name}/${repo}.git"
   } else {
-     git credentialsId: '${cred}', url: 'https://github.com/${name}/${repo}.git'
+     git credentialsId: "${cred}", url: "https://github.com/${name}/${repo}.git"
   }
 }
--- vars/checkOutGitSSH.groovy
+++ vars/checkOutGitSSH.groovy
@@ -4,6 +4,6 @@ def call(String branch = null, String cred, String name, String repo){
   if(branch != null){
     git branch: "${branch}", credentialsId: "${cred}", url: "git@github.com:${name}/${repo}.git"
   } else {
-    git credentialsId: '${cred}', url: "git@github.com:${name}/${repo}.git"
+    git credentialsId: "${cred}", url: "git@github.com:${name}/${repo}.git"
   }
 }
