c84798c258ede96ca6fe184ad81317c6563d53a3
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-27 12:39:14 +0200

    modi var

--- vars/dumpXampp.groovy
+++ vars/dumpXampp.groovy
@@ -1,7 +1,7 @@
 #!C:\devops\Groovy-3.0.0\bin groovy
 // vars/dumpXampp
 def call(String user, String host, String port, String dbname, String dumpDir, String pass = null) {
-  if(pass = null){
+  if(pass == null){
     sh "cd /c/xampp/mysql/bin && ./mysqldump --user=${user} --host=${host} --protocol=tcp --port=${port} --default-character-set=utf8 ${dbname} > ${dumpDir}"
   }else{
     sh "cd /c/xampp/mysql/bin && ./mysqldump --user=${user} --password=${pass} --host=${host} --protocol=tcp --port=${port} --default-character-set=utf8 ${dbname} > ${dumpDir}"
