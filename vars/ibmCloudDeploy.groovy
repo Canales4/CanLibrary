7523b1a4f0932759944d3345003c90503f413f38
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-28 09:41:48 +0200

    Cambios libreria

--- vars/ibmCloudDeploy.groovy
+++ vars/ibmCloudDeploy.groovy
@@ -2,6 +2,8 @@
 // vars/imbCloudDeploy
 
 //this var will only work if the client and the server go separator in the same directory
-def call(String app, String apikey) {
-  sh "ibmcloud login --apikey ${apikey} && ibmcloud target --cf && cd client && ibmcloud cf push ${app}"
+def call(String app) {
+  withCredentials([string(credentialsId: 'ApiKey', variable: 'varApiKey')]) {
+    sh "ibmcloud login --apikey $varApikey && ibmcloud target --cf && cd client && ibmcloud cf push ${app}"
+  }
 }
